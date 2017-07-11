bdbc12ab312f808be89c0ac9a47f93c4ceb0aebc
elipmoc <relipmoc101@gmail.com>
2017-07-12 01:24:04 +0900

    practicellvm名前空間追加

--- APP.hpp
+++ APP.hpp
@@ -1,8 +1,9 @@
 #pragma once
-
-//安全な削除
-template<class T>
-void Safe_Delete(T&& t){delete t;t=nullptr;}
-//安全な削除（配列版）
-template<class T>
-void Safe_Delete_Array(T&& t){delete[] t;t=nullptr;}
+namespace practicellvm{
+    //安全な削除
+    template<class T>
+    void Safe_Delete(T&& t){delete t;t=nullptr;}
+    //安全な削除（配列版）
+    template<class T>
+    void Safe_Delete_Array(T&& t){delete[] t;t=nullptr;}
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "APP.hpp"
 #include "Token.hpp"
+using practicellvm::Token;
+using practicellvm::TokenType;
 int main(){
     Token token("114514",TokenType::TOK_DIGIT,15);
     std::cout<<static_cast<int>(token.GetTokenType())<<std::endl;
