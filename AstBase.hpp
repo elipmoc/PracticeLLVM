6f23bc1936352c125117829f51fc8ac4dd1c6d66
elipmoc <relipmoc101@gmail.com>
2017-07-22 00:16:42 +0900

    リスト5.9 変数宣言を表すASTまで

--- AstBase.hpp
+++ AstBase.hpp
@@ -1,13 +1,59 @@
+#pragma once
+#include <string>
+
+namespace practicellvm{
+
 //ASTの種類
 enum class AstID{
-    BaseId
+    BaseId,
+    VariableDeclID,
+    BinaryExprID,
+    CallExprID,
+    JumpStmtID,
+    VariableID,
+    NumberID
 };
+
 //ASTの基底クラス
 class BaseAST{
     AstID ID;
     public:
     BaseAST(AstID id):ID(id){}
     virtual ~BaseAST(){}
-    AstID getValueID()const{return ID;}
+    AstID GetValueID()const{return ID;}
 };
 
+//変数宣言を表すAST
+class VariableDeclAST:public BaseAST{
+    public:
+    //変数の宣言型識別
+    enum class DeclType{
+        param,//引数
+        local//ローカル変数
+    };
+    private:
+    DeclType type;
+    std::string name;
+    public:
+    VariableDeclAST(const std::string& _name):BaseAST(AstID::VariableID),name(_name){
+
+    }
+
+    static bool classof(VariableDeclAST const*){return true;}
+    static bool classof(BaseAST const* base){
+        return base->GetValueID()==AstID::VariableDeclID;
+    }
+    ~VariableDeclAST(){}
+
+    //変数名を取得する
+    std::string GetName(){return name;}
+
+    //変数の宣言種別を設定する
+    bool SetDeclType(DeclType _type){type=_type;return true;}
+
+    //変数の宣言種別を取得する
+    DeclType GetType(){return type;}
+
+};
+
+}
