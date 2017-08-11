02d9dcece108d62fb3b29a4c50ab4aa40a6bc8fe
elipmoc <relipmoc101@gmail.com>
2017-08-11 19:40:19 +0900

    リスト　5.37

--- CodeGen.cpp
+++ CodeGen.cpp
@@ -4,7 +4,7 @@ namespace practicellvm{
     //コンストラクタ
     CodeGen::CodeGen(){
         builder=new llvm::IRBuilder<>(m_context);
-        mod=nullptr;
+        m_mod=nullptr;
     }
 
     //コード生成実行
@@ -16,7 +16,7 @@ namespace practicellvm{
 
     //Module取得
     llvm::Module &CodeGen::GetModule(){
-        return mod? *mod : *(new llvm::Module("null",m_context));
+        return m_mod? *m_mod : *(new llvm::Module("null",m_context));
     }
 
     //Module生成メソッド
@@ -25,14 +25,14 @@ namespace practicellvm{
     bool CodeGen::GenerateTranslationUnit(TranslationUnitAST& tunit,const std::string& name){
         
         //Moduleを生成
-        mod=new llvm::Module(name,m_context);
+        m_mod=new llvm::Module(name,m_context);
 
         //function declaration
         for(int i=0;;i++){
             auto proto=tunit.GetPrototype(i);
             if(!proto)break;
-            if(!GeneratePrototype(proto,mod)){
-                Safe_Delete(mod);
+            if(!GeneratePrototype(proto,m_mod)){
+                Safe_Delete(m_mod);
                 return false;
             }
         }
@@ -41,11 +41,51 @@ namespace practicellvm{
         for(int i=0;;i++){
             FunctionAST* func=tunit.GetFunction(i);
             if(!func)break;
-            if(!GenerateFunctionDefinition(func,mod)){
-                Safe_Delete(mod);
+            if(!GenerateFunctionDefinition(func,m_mod)){
+                Safe_Delete(m_mod);
                 return false;
             }
         }
         return true;
+    }
+
+    //関数宣言生成メソッド
+    //<param> PrototypeAST ,Module
+    //<return> 生成したllvm::Functionのポインタ
+    llvm::Function* CodeGen::GeneratePrototype(PrototypeAST* proto,llvm::Module* mod){
+        //既に宣言済みかチェック
+        llvm::Function* func=mod->getFunction(proto->GetName());
+        if(func){
+            if(func->arg_size()==proto->GetParamNum() && func->empty())
+                return func;
+            fprintf(stderr,"error::function %s is redefined",proto->GetName().c_str());
+            return nullptr;
+        }
+
+        //引数を生成
+        std::vector<llvm::Type*> int_types(
+                proto->GetParamNum(),llvm::Type::getInt32Ty(m_context)
+            );
+        //関数のシグネチャ生成
+        llvm::FunctionType* func_type=
+            llvm::FunctionType::get(
+                llvm::Type::getInt32Ty(m_context),
+                int_types,false
+            );
+
+        //関数生成
+        func=
+            llvm::Function::Create(
+                func_type,llvm::Function::ExternalLinkage,proto->GetName(),mod
+            );
+        
+        //引数の名前を設定する
+        auto itr=func->arg_begin();
+        for(int i=0;i<proto->GetParamNum();i++){
+            itr->setName(proto->GetParamName(i).append("_arg"));
+            ++itr;
+        }
+        return func;
+
     }
 }
--- CodeGen.hpp
+++ CodeGen.hpp
@@ -15,7 +15,7 @@ namespace practicellvm{
         //現在コード生成中のFunction
         llvm::Function *curFunc;
         //生成したModuleを格納
-        llvm::Module *mod;
+        llvm::Module *m_mod;
         //LLVM-IRを生成するIRBuilderクラス
         llvm::IRBuilder<> *builder;
         public:
@@ -25,16 +25,16 @@ namespace practicellvm{
         llvm::Module &GetModule();
         private:
 		bool GenerateTranslationUnit(TranslationUnitAST &tunit,const std::string& name);
-		llvm::Function *GenerateFunctionDefinition(FunctionAST *func, llvm::Module *mod);
-		llvm::Function *GeneratePrototype(PrototypeAST *proto, llvm::Module *mod);
-		llvm::Value *GenerateFunctionStatement(FunctionStmtAST *func_stmt);
-		llvm::Value *GenerateVariableDeclaration(VariableDeclAST *vdecl);
-		llvm::Value *GenerateStatement(BaseAST *stmt);
-		llvm::Value *GenerateBinaryExpression(BinaryExprAST *bin_expr);
-		llvm::Value *GenerateCallExpression(CallExprAST *call_expr);
-		llvm::Value *GenerateJumpStatement(JumpStmtAST *jump_stmt);
-		llvm::Value *GenerateVariable(VariableAST *var);
-		llvm::Value *GenerateNumber(const int value);
+		llvm::Function* GenerateFunctionDefinition(FunctionAST *func, llvm::Module *mod);
+		llvm::Function* GeneratePrototype(PrototypeAST *proto, llvm::Module *mod);
+		llvm::Value* GenerateFunctionStatement(FunctionStmtAST *func_stmt);
+		llvm::Value* GenerateVariableDeclaration(VariableDeclAST *vdecl);
+		llvm::Value* GenerateStatement(BaseAST *stmt);
+		llvm::Value* GenerateBinaryExpression(BinaryExprAST *bin_expr);
+		llvm::Value* GenerateCallExpression(CallExprAST *call_expr);
+		llvm::Value* GenerateJumpStatement(JumpStmtAST *jump_stmt);
+		llvm::Value* GenerateVariable(VariableAST *var);
+		llvm::Value* GenerateNumber(const int value);
         bool LinkModule(llvm::Module *dest,const std::string& file_name);
     };
 }
