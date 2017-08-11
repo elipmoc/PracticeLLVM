cd30b573c4f87b812b843631db685c8eaabb5deb
elipmoc <relipmoc101@gmail.com>
2017-08-11 18:56:21 +0900

    リスト　5.36

--- CodeGen.cpp
+++ CodeGen.cpp
@@ -15,7 +15,37 @@ namespace practicellvm{
     }
 
     //Module取得
-   llvm::Module &CodeGen::GetModule(){
+    llvm::Module &CodeGen::GetModule(){
         return mod? *mod : *(new llvm::Module("null",m_context));
+    }
+
+    //Module生成メソッド
+    //<param> TranslationUnitAST Module名（入力ファイル名）
+    //<return> 成功時：true　失敗時:false
+    bool CodeGen::GenerateTranslationUnit(TranslationUnitAST& tunit,const std::string& name){
+        
+        //Moduleを生成
+        mod=new llvm::Module(name,m_context);
+
+        //function declaration
+        for(int i=0;;i++){
+            auto proto=tunit.GetPrototype(i);
+            if(!proto)break;
+            if(!GeneratePrototype(proto,mod)){
+                Safe_Delete(mod);
+                return false;
+            }
+        }
+
+        //function definition
+        for(int i=0;;i++){
+            FunctionAST* func=tunit.GetFunction(i);
+            if(!func)break;
+            if(!GenerateFunctionDefinition(func,mod)){
+                Safe_Delete(mod);
+                return false;
+            }
+        }
+        return true;
     }
 }
