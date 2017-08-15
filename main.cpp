81b6b4159d9010eff9a5418d31aa9201d78e7b87
elipmoc <relipmoc101@gmail.com>
2017-08-15 22:15:16 +0900

    リスト　5.50

--- main.cpp
+++ main.cpp
@@ -14,9 +14,48 @@ int main(int argc,char** argv){
     OptionParser opt(argc,argv);
     if(!opt.ParseOption())
         exit(1);
-    std::cout<<"aaa";
-    /*TokenStream* tokenStream;
-    tokenStream=LexicalAnalysis("test.txt");
+
+    //check
+	if(opt.getInputFileName().length()==0){
+		fprintf(stderr,"入力ファイル名が指定されていません\n");
+		exit(1);
+	}
+
+	//lex and parse
+	Parser *parser=new Parser(opt.GetInputFileName());
+	if(!parser->DoParse()){
+		fprintf(stderr, "err at parser or lexer\n");
+		SAFE_DELETE(parser);
+		exit(1);
+	}
+
+	//get AST
+	TranslationUnitAST &tunit=parser->GetAST();
+	if(tunit.empty()){
+		fprintf(stderr,"TranslationUnit is empty");
+		SAFE_DELETE(parser);
+		exit(1);
+	}
+
+	CodeGen *codegen=new CodeGen();
+	if(!codegen->DoCodeGen(tunit, opt.GetInputFileName())){
+		fprintf(stderr, "err at codegen\n");
+		SAFE_DELETE(parser);
+		SAFE_DELETE(codegen);
+		exit(1);
+	}
+
+	//get Module
+	llvm::Module& mod=codegen->GetModule();
+	if(mod.empty()){
+		fprintf(stderr,"Module is empty");
+		SAFE_DELETE(parser);
+		SAFE_DELETE(codegen);
+		exit(1);
+    }
+
+  /*  TokenStream* tokenStream;
+    tokenStream=LexicalAnalysis("sample.dc");
     if(tokenStream==nullptr){
         std::cout<<"NullPtr!!"<<std::endl;
         return 0;
